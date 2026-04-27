a7ac308611651e804ec1b14118db64ff0b22c93b
Alessandro Tecce <root@atec.pub>
2026-04-26 21:27:33 -0500

    import Foundation NSLinguisticTagger

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import Bible
+import Foundation
 
 func arcGenealogy(daemon: ArcDaemon?) {
 	var cur = daemon!.father()
@@ -10,6 +11,20 @@ func arcGenealogy(daemon: ArcDaemon?) {
 	}
 }
 
+let text = "John baptizes with water and Jesus baptizes with fire"
+let tagger = NSLinguisticTagger(tagSchemes: [.nameType], options: 0)
+tagger.string = text
+let range = NSRange(location: 0, length: text.utf16.count)
+let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
+let tags: [NSLinguisticTag] = [.personalName]
+tagger.enumerateTags(in: range, unit: .word, scheme: .nameType, options: options) {
+	tag, tokenRange, stop in
+
+	if let tag = tag, tags.contains(tag) {
+		let name = (text as NSString).substring(with: tokenRange)
+		print("\(name): \(tag)")
+	}
+}
 
 let יֵשׁוּ = arcMorningStar()
 
